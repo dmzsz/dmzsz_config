240665078f86de15e0987f1a5281e4203769a40a
dmzsz <dmzsz@sina.com>
2015-06-11 16:52:36 +0800

    first commit

---
+++ setup_exvim.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+export exvim_home=/opt/exvim
+if [ ! -d $exvim_home ]; then
+  mkdir  $exvim_home
+# else
+#   rm -rf $exvim_home
+#   mkdir  $exvim_home
+fi
+git clone https://github.com/exvim/main $exvim_home
+cd $exvim_home
+sh unix/install.sh
+
+if [ ! -e ~/.vimrc ]; then
+  touch ~/.vimrc
+  chown -R gen:gen ~/.vimrc
+else
+  # rm ~/.vimrc
+  # touch ~/.vimrc
+  # chown -R gen:gen ~/.vimr
+fi
+
+cat > ~/.vimrc << EOF
+let g:exvim_custom_path='$exvim_home/'
+source $exvim_home/.vimrc
+EOF
+
+apt-get install ctags
---
+++ setup_ubuntu_mongodb3.0.sh
@@ -0,0 +1,13 @@
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+sudo service mongod start
+# sudo service mongod stop
+# sudo service mongod restart
+
+# Remove any MongoDB packages that you had previously installed.
+# sudo apt-get purge mongodb-org*
+# Remove MongoDB databases and log files.
+# sudo rm -r /var/log/mongodb
+# sudo rm -r /var/lib/mongodb
